26bf402513c111d7d24382e411e9ab9e795522df
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2023-01-08 19:53:51 -0300

    ItemList component and style updated

---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,12 @@
+import Item from "../Item/Item";
+import './ItemList.css';
+
+const ItemList = ({productos}) => {
+    return(
+        <div className="item-list">
+            {productos.map((prod) => <Item key={prod.id} {...prod}/>)}
+        </div>
+    )
+}
+
+export default ItemList
