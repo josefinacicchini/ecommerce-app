7b29140cdf391383d8b584ba91159005e3b51363
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2023-01-08 19:52:35 -0300

    ItemListContainer component and style updated

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
+import { pedirDatos } from '../../helpers/dataRequest';
+import ItemList from '../ItemList/ItemList';
 import './ItemListContainer.css';
 
 const ItemListContainer = (props) => {
+    const [productos, setProductos] = useState([])
+   
+    useEffect(()=>{
+        pedirDatos()
+            .then((res)=>{
+                setProductos(res)
+            })
+            .catch((err)=> {
+                console.log(err)
+            })
+    }, [])
 
     return (
-    <div className="item-list-container">
-        <h1 style={{color:'#423f32', fontFamily: 'roboto', fontWeight: 'lighter',fontSize: '1.1vw'}}>
-            {props.greeting}
-        </h1>
-    </div>
+        <div className="item-list-container">
+            <ItemList productos={productos}/>
+        </div>
     )
 
 }
