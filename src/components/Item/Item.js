6aadf8d408f79df9802f2b6461158727fd7bc42e
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2023-01-09 21:32:52 -0300

    Link to detail added

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,12 +1,13 @@
 import './Item.css';
+import { Link } from 'react-router-dom';
 
-const Item = ({name,image,price}) => {
+const Item = ({name,image,price,id}) => {
     return (
         <div className="item">
                 <img src={image} alt={name}/>
-                <p>{name}</p>
+                <p className='product-name'>{name}</p>
                 <h4>{new Intl.NumberFormat('en-US', {style: 'currency', currency: 'ARS'}).format(price)}</h4>
-
+                <Link className="link-to-detail" to={`/detail/${id}`}>Ver más</Link>
         </div>
     )
 }
