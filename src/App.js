4347877719da9ce18cadf0877f6da9f8687c8be8
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2023-01-09 21:54:55 -0300

    Routing through different sections added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import ItemCount from "./components/ItemCount/ItemCount";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Error404 from "./components/Error404/Error404";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <ItemListContainer greeting = 'Bienvenido a nuestra tienda online!'/>
-        <ItemCount />
+      <BrowserRouter>
+        
+        <NavBar />
+        
+        <Routes>
+          <Route path="/" element={ <ItemListContainer /> } />
+          <Route path="/detail/:itemId" element={<ItemDetailContainer />} />
+          <Route path="/productos/:categoryId" element={ <ItemListContainer /> }/>
+          <Route path="*" element={ <Error404/> }/>
+
+        </Routes>
+        
+      </BrowserRouter>
+
     </div>
   );
 }
