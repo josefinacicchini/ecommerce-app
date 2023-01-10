5a8781d7741a867b40178b3c9f1d422cc5ea2050
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2023-01-09 21:43:37 -0300

    CategoryId navigation param added

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,19 +2,27 @@ import { useEffect, useState } from 'react';
 import { pedirDatos } from '../../helpers/dataRequest';
 import ItemList from '../ItemList/ItemList';
 import './ItemListContainer.css';
+import { useParams } from 'react-router-dom';
+
 
 const ItemListContainer = (props) => {
     const [productos, setProductos] = useState([])
+    const {categoryId} = useParams()
+    console.log(categoryId)
    
     useEffect(()=>{
         pedirDatos()
             .then((res)=>{
-                setProductos(res)
+                if(categoryId) {
+                setProductos(res.filter(prod => prod.category === categoryId))
+                } else {
+                    setProductos(res)
+                }
             })
             .catch((err)=> {
                 console.log(err)
             })
-    }, [])
+    }, [categoryId])
 
     return (
         <div className="item-list-container">
