b373458fe03565cace4f171f8d58a2f6fb148675
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2023-01-09 21:35:56 -0300

    return to previous page button added with useNavigate hook

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,17 +1,24 @@
 import ItemCount from "../ItemCount/ItemCount"
 import './ItemDetail.css'
+import { useNavigate } from "react-router-dom"
 
-// const ItemDetail = ({id, name, image, description, price}) => {
 const ItemDetail = ({item}) => {
+    const navigate = useNavigate()
+    const handleVolver = () => {
+        navigate(-1)
+    }
+
     return (
         <div className="item-detail">
             <img src={item.image} alt={item.name} />
             <h2>{item.name}</h2>
-            <p>{item.description}</p>
+            <p className="description">{item.description}</p>
             <p>Categoría: {item.category}</p>
             <h3>{new Intl.NumberFormat('en-US', {style: 'currency', currency: 'ARS'}).format(item.price)}</h3>
             
             <ItemCount item={item} />
+
+            <button className='return-button' onClick={handleVolver}>Volver</button>
         </div>
         
     )
