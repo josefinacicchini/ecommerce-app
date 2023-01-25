3b26bf8d0970f424d7d3a60b80de3a38cd7d8710
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2023-01-25 17:43:55 -0300

    login information, logout button and styles added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,56 +7,65 @@ import { IoPaw } from "react-icons/io5";
 import { BiTennisBall, BiBone } from "react-icons/bi";
 import { RiZzzFill } from "react-icons/ri";
 import { GiSoap } from "react-icons/gi";
+import { useLoginContext } from '../../context/LoginContext';
 
 const NavBar = () => {
+    const {user, logout} = useLoginContext()
+
     return(
-        <header className="header-container">
-            <div className="brand">
-                <img src={image} alt="brand logo" />
-                <Link className="brand-name" to={'/'}>Pawsome</Link>
-            </div>
-       
-            <nav className="nav-container">
-                <div className="links-container">
-                    
-                    <Tooltip title="Paseo">
-                        <Link className="nav-link" to={'/productos/Paseo'}>
-                            <IoPaw/>
-                        </Link>
-                    </Tooltip>
-
-                    <Tooltip title="Alimentación">
-                        <Link className="nav-link" to={'/productos/Alimentación'}>
-                            <BiBone/>
-                        </Link>
-                    </Tooltip>
-
-                    <Tooltip title="Descanso">
-                        <Link className="nav-link" to={'/productos/Descanso'}>
-                            <RiZzzFill/>
-                        </Link>
-                    </Tooltip>
-
-                    <Tooltip title="Higiene">
-                        <Link className="nav-link" to={'/productos/Higiene'}>
-                            <GiSoap/>
-                        </Link>
-                    </Tooltip>
-
-                    <Tooltip title="Juego">
-                        <Link title="juego" className="nav-link" to={'/productos/Juego'}>
-                            <BiTennisBall/>
-                        </Link>
-                    </Tooltip>
-
-                    {/* <Link className="nav-link" to={'/'}>HOME</Link> */}
-                    {/* <Link className="nav-link" to={'/contact'}>CONTACT</Link> */}
-
-
+        <div className='navbar-container'>
+            <header className="header-container">
+                <div className="brand">
+                    <img src={image} alt="brand logo" />
+                    <Link className="brand-name" to={'/'}>Pawsome</Link>
                 </div>
-                <CartWidget />
-            </nav>
-        </header>
+        
+                <nav className="nav-container">
+                    <div className="links-container">
+                        
+                        <Tooltip title="Paseo">
+                            <Link className="nav-link" to={'/productos/Paseo'}>
+                                <IoPaw/>
+                            </Link>
+                        </Tooltip>
+
+                        <Tooltip title="Alimentación">
+                            <Link className="nav-link" to={'/productos/Alimentación'}>
+                                <BiBone/>
+                            </Link>
+                        </Tooltip>
+
+                        <Tooltip title="Descanso">
+                            <Link className="nav-link" to={'/productos/Descanso'}>
+                                <RiZzzFill/>
+                            </Link>
+                        </Tooltip>
+
+                        <Tooltip title="Higiene">
+                            <Link className="nav-link" to={'/productos/Higiene'}>
+                                <GiSoap/>
+                            </Link>
+                        </Tooltip>
+
+                        <Tooltip title="Juego">
+                            <Link className="nav-link" to={'/productos/Juego'}>
+                                <BiTennisBall/>
+                            </Link>
+                        </Tooltip>
+
+                        {/* <Link className="nav-link" to={'/'}>HOME</Link> */}
+                        {/* <Link className="nav-link" to={'/contact'}>CONTACT</Link> */}
+
+
+                    </div>
+                    <CartWidget />
+                </nav>
+            </header>
+            <div className='login-logout-container'>
+                        <p>Bienvenido {user.email}</p>
+                        <button className='logout-button' onClick={logout}>Logout</button>
+            </div>
+        </div>
     )
 }
 
