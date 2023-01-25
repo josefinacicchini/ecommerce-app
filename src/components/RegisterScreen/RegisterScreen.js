08a7f131812e32a390b27acd48dacdb9c7d71be8
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2023-01-25 17:46:08 -0300

    register screen added

---
+++ src/components/RegisterScreen/RegisterScreen.js
@@ -0,0 +1,56 @@
+import './RegisterScreen.css'
+import { useState, useContext } from 'react'
+import { LoginContext } from '../../context/LoginContext'
+import { Link } from 'react-router-dom'
+
+const RegisterScreen = () => {
+    const { user, register } = useContext(LoginContext)
+
+    const [values, setValues]= useState({
+        email: '',
+        password: ''
+    })
+
+    const handleInputChange = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        register(values)
+    }
+
+    return (
+        <div className='login-screen'>
+            <div className='login-form-container'>
+                <h2>Register</h2>
+                <form className='login-form' onSubmit={handleSubmit}>
+                    <input 
+                        className='form-input' 
+                        type={'email'} 
+                        value={values.email} 
+                        onChange={handleInputChange}
+                        name='email'  
+                    />
+                    <input 
+                        className='form-input' 
+                        type={'password'} 
+                        value={values.password} 
+                        onChange={handleInputChange}
+                        name='password'
+                    />
+                    <button className='login-form-button'>Registrarme</button>
+
+                    { user.error && <p className='error'>{user.error}</p>}
+
+                </form>
+                <Link to='/login'>Ya estoy registrado</Link>
+            </div>
+        </div>
+    )
+}
+
+export default RegisterScreen
