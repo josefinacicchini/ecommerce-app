4e91205d57192c331cde8a7f44dfa93426433ddb
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2023-01-25 17:52:06 -0300

    Firebase data base and auth configuration

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApqe9DbfDVRoAsOo8OcRZKnH8x82vgV6Q",
+  authDomain: "ecommerce-pawsome.firebaseapp.com",
+  projectId: "ecommerce-pawsome",
+  storageBucket: "ecommerce-pawsome.appspot.com",
+  messagingSenderId: "336208744391",
+  appId: "1:336208744391:web:ae81a1a22d12e58af893e2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+
+export const auth = getAuth(app)
+// export const provider = new GoogleAuthProvider()
---
+++ src/firebase/upload.js
@@ -0,0 +1,5 @@
+// import {db} from './config.js'
+// import MOCK from '../data/MOCK_DATA.json' assert {type:'json'}
+
+
+// console.log(MOCK)
