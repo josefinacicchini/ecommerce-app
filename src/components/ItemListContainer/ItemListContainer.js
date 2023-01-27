62fb4359e7dc2593456acab6bc1aeb4552b911c2
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2023-01-27 13:57:56 -0300

    Petitions to Firebase database added

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,27 +1,47 @@
 import { useEffect, useState } from 'react';
-import { pedirDatos } from '../../helpers/dataRequest';
+// import { pedirDatos } from '../../helpers/dataRequest';
 import ItemList from '../ItemList/ItemList';
 import './ItemListContainer.css';
 import { useParams } from 'react-router-dom';
-
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../../firebase/config';
 
 const ItemListContainer = (props) => {
     const [productos, setProductos] = useState([])
     const {categoryId} = useParams()
-    console.log(categoryId)
    
     useEffect(()=>{
-        pedirDatos()
-            .then((res)=>{
-                if(categoryId) {
-                setProductos(res.filter(prod => prod.category === categoryId))
-                } else {
-                    setProductos(res)
-                }
+        // pedirDatos()
+        //     .then((res)=>{
+        //         if(categoryId) {
+        //         setProductos(res.filter(prod => prod.category === categoryId))
+        //         } else {
+        //             setProductos(res)
+        //         }
+        //     })
+        //     .catch((err)=> {
+        //         console.log(err)
+        //     })
+
+        //referencia a la base de datos
+        const productosRef = collection(db, "productos")
+        const q = categoryId
+                    ? query(productosRef, where("category", "==", categoryId))
+                    : productosRef
+        //peticion asincronica
+        
+        getDocs(q)
+            .then((resp)=> {
+                setProductos(resp.docs.map((doc)=> {
+                    return { 
+                        ...doc.data(), 
+                        id: doc.id
+                    }
+                }))
             })
-            .catch((err)=> {
-                console.log(err)
-            })
+
+
+
     }, [categoryId])
 
     return (
