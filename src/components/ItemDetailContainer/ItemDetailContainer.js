6283e5dac9dab8569c9f2c54565e2012ab3899ee
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2023-01-27 13:56:39 -0300

    Firebase data base configuration added

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,28 +1,37 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { pedirItemPorId } from "../../helpers/dataRequest"
+// import { pedirItemPorId } from "../../helpers/dataRequest"
 import ItemDetail from "../ItemDetail/ItemDetail"
 import './ItemDetailContainer.css'
+import { doc, getDoc} from "firebase/firestore"
+import { db } from "../../firebase/config"
 
 const ItemDetailContainer = () => {
     const [item, setItem] = useState([])
     const {itemId} = useParams()
 
     useEffect(() => {
-        pedirItemPorId(parseInt(itemId))
-        .then((data) => {
-            setItem(data)
-        })
-        .catch(error => {
-            console.log(error)
-        })
+        // pedirItemPorId(parseInt(itemId))
+        // .then((data) => {
+        //     setItem(data)
+        // })
+        // .catch(error => {
+        //     console.log(error)
+        // })
+        //referencia
+        const docRef = doc(db, 'productos', itemId)
+        //peticion async
+        getDoc(docRef)
+            .then((doc)=>
+            setItem({...doc.data(), id: doc.id}
+            ))
 
     }, [itemId])
 
     return(
         <div className="item-detail-container">
             {item && 
-            <ItemDetail item={item} />
+            <ItemDetail {...item} />
             }
         </div>
     )
